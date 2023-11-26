f2a70500af82584da299c133bb2f94e5cf7563e8
kimkevin90 <kimkevin90@naver.com>
2023-11-26 14:31:57 +0900

    using ConversationSummaryMemory

--- chapter2/main.py
+++ chapter2/main.py
@@ -6,11 +6,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-chat = ChatOpenAI()
+chat = ChatOpenAI(verbose=True)
 
+'''
 # memory_key='messages': 이 파라미터는 메모리 시스템에서 대화 내용을 저장하거나 접근할 때 사용되는 키(key)를 지정합니다. 여기서 'messages'는 대화의 텍스트 내용을 저장하고 참조하는 데 사용되는 키입니다. 즉, 이 키를 통해 대화 내용에 접근하거나 수정할 수 있습니다.
 # return_messages=True: 이 설정은 메모리 시스템이 작동할 때 대화 내용을 반환할지 여부를 결정합니다. return_messages=True로 설정하면, 메모리 시스템이 대화 내용을 반환하며, 이를 통해 대화에 적절하게 응답하거나 필요한 정보를 추출하는 데 사용할 수 있습니다.
-memory = ConversationBufferMemory(memory_key='messages', return_messages=True)
+memory = ConversationBufferMemory(
+    # 대화 내용을 messages.json에 기록
+    # chat_memory=FileChatMessageHistory("messages.json"),
+    memory_key='messages', 
+    return_messages=True
+)
+'''
+
+# ConversationSummaryMemory는 대화의 핵심 내용과 요약된 정보를 저장합니다. 이는 대화의 전체 내용을 저장하는 것이 아니라, 중요한 정보나 결정적인 대화 포인트를 요약하여 저장합니다.
+memory = ConversationSummaryMemory(
+    memory_key='messages', 
+    return_messages=True,
+    llm=chat
+)
+
 
 prompt = ChatPromptTemplate(
     input_variables=["content", "messages"],
@@ -26,6 +41,7 @@ chain = LLMChain(
     llm=chat,
     prompt=prompt,
     memory=memory,
+    verbose=True
 )
 
 while True:
