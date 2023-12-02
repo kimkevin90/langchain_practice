84484e88904e88594ffc94e3b3c3b0834ed28695
kimkevin90 <kimkevin90@naver.com>
2023-12-02 13:32:34 +0900

    refactoring build_chat

--- chapter5/pdf-dist/app/chat/chat.py
+++ chapter5/pdf-dist/app/chat/chat.py
@@ -2,13 +2,30 @@ import random
 from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
 from app.chat.vector_stores import retriever_map
-from app.chat.llms.chatopenai import build_llm
-from app.chat.memories.sql_memory import build_memory
+from app.chat.llms import llm_map
+from app.chat.memories import memory_map
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 from app.web.api import (
     set_conversation_components,
     get_conversation_components
 )
+
+def select_component(
+    component_type, component_map, chat_args
+):
+    components = get_conversation_components(
+        chat_args.conversation_id
+    )
+    previous_component = components[component_type]
+
+    if previous_component:
+        builder = component_map[previous_component]
+        return previous_component, builder(chat_args)
+    else:
+        random_name = random.choice(list(component_map.keys()))
+        builder = component_map[random_name]
+        return random_name, builder(chat_args)
+
 
 def build_chat(chat_args: ChatArgs):
     """
@@ -21,32 +38,34 @@ def build_chat(chat_args: ChatArgs):
 
         chain = build_chat(chat_args)
     """
-    components = get_conversation_components(
-        chat_args.conversation_id
+
+    retriever_name, retriever = select_component(
+        "retriever",
+        retriever_map,
+        chat_args
     )
-    previous_retriever = components["retriever"]
 
-    retriever = None
-    if previous_retriever:
-        # DB에 저장된 retriever 사용
-        build_retriever = retriever_map[previous_retriever]
-        retriever = build_retriever(chat_args)
-    else:
-        # 무작위 retriever 생성 후 사용
-        random_retriever_name = random.choice(list(retriever_map.keys()))
-        build_retriever = retriever_map[random_retriever_name]
-        retriever = build_retriever(chat_args)
-        set_conversation_components(
-            conversation_id=chat_args.conversation_id,
-            llm="",
-            memory="",
-            retriever=random_retriever_name
-        )
+    llm_name, llm = select_component(
+        "llm",
+        llm_map,
+        chat_args
+    )
+    memory_name, memory = select_component(
+        "memory",
+        memory_map,
+        chat_args
+    )
 
-
-    retriever = build_retriever(chat_args)
-    llm = build_llm(chat_args)
-    memory = build_memory(chat_args)
+    print(
+        f"Running chain with: memory {memory_name}, llm: {llm_name}, retriever: {retriever_name}"
+    )
+    set_conversation_components(
+        chat_args.conversation_id,
+        llm=llm_name,
+        retriever=retriever_name,
+        memory=memory_name
+    )
+    
 
     # ConversationalRetrievalChain을 사용하여 대화형 검색 체인을 구성하는 과정을 보여줍니다. 
     # 이 체인은 검색(retriever), 언어 모델(llm), 그리고 메모리 시스템(memory)을 통합하여 사용자의 질문에 답변을 생성합니다.
